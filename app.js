3856d93ed47ba02db1b223390887163fac0833e8
PandwarAbhi <103624202+PandwarAbhi@users.noreply.github.com>
2022-11-12 19:12:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const fetchPrice= async(ctype) =>{
      const volume  = r.data.coin.volume;
      const change = r.data.coin.priceChange1d;
      const base = r.data.coin.name;
-     const target = 'USD';
+     const target = 'INR';
 
 
      res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
