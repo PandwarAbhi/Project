ab74ac16eebea2c5f78cf0115bdb128d93709d8a
PandwarAbhi <103624202+PandwarAbhi@users.noreply.github.com>
2022-11-12 19:14:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ form.addEventListener('submit',(e)=>{
 
 
 const fetchPrice= async(ctype) =>{
-    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=INR`);
+    const r = await axios.get(`https://api.coinstats.app/public/v1/coins/${ctype}?currency=USD`);
     console.log(r.data.coin.price);
      const price = r.data.coin.price;
      const volume  = r.data.coin.volume;
      const change = r.data.coin.priceChange1d;
      const base = r.data.coin.name;
-     const target = 'INR';
+     const target = 'USD';
 
 
      res.innerHTML =`<tr style="background-color:blue; color:white; font-weight:700">
